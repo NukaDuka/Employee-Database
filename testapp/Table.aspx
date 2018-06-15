872cbde8d20fef517f7d838f155c895b1f061ddc
Kartik Gokte <ksgokte@gmail.com>
2018-06-15 23:06:10 +0530

    finished backend for Table.aspx

--- testapp/Table.aspx
+++ testapp/Table.aspx
@@ -1,8 +1,24 @@
-﻿<%@ Page Title="View Employee Data" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Table.aspx.cs" Inherits="testapp._Default" %>
+﻿<%@ Page Title="View Employee Data" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Table.aspx.cs" Inherits="testapp.Table" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
     <div class="container-fluid">
         <h2 class="text-center">View Employee table</h2>
-        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+        <asp:GridView ID="emptable" runat="server" AutoGenerateColumns="false"
+	AllowSorting="True" AllowPaging="True"
+    PageSize="4" OnRowDataBound="OnRowDataBound">
+            <Columns>
+                <asp:BoundField DataField="EMP_ID" HeaderText="Employee ID" />
+                <asp:BoundField DataField="EMP_FNAME" HeaderText="First Name" />
+                <asp:BoundField DataField="EMP_LNAME" HeaderText="Last Name" />
+                <asp:BoundField DataField="EMP_EMAIL" HeaderText="Email ID" />
+                <asp:BoundField DataField="EMP_GENDER" HeaderText="Gender" />
+                <asp:BoundField DataField="EMP_QUALIFICATION" HeaderText="Qualifications" />
+                <asp:TemplateField HeaderText="Image">
+                    <ItemTemplate>
+                        <asp:Image ID="imgemp" runat="server" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
     </div>
 </asp:Content>  
--- testapp/Table.aspx.cs
+++ testapp/Table.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,16 +10,45 @@ using System.Web.UI.WebControls;
 
 namespace testapp
 {
-    public partial class Table : System.Web.UI.Page
+    public partial class Table : Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        private void BindGrid()
         {
-
+            string constr = ConfigurationManager.ConnectionStrings["EmployeeConnectionString"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(constr))
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM Employee", conn))
+                {
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+                    emptable.DataSource = dt;
+                    emptable.DataBind();
+                }
+            }
         }
 
-        protected void table_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        protected void Page_Load(object sender, EventArgs e)
         {
+            if (!this.IsPostBack)
+            {
+                this.BindGrid();
+            }
+        }
 
+        protected void OnRowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                DataRowView dr = (DataRowView)e.Row.DataItem;
+                string imageUrl = "data:" + dr["EMP_FILETYPE"] + ";base64," + Convert.ToBase64String((byte[])dr["EMP_IMAGE"]);
+                (e.Row.FindControl("imgemp") as Image).ImageUrl = imageUrl;
+            }
+        }
+
+        protected void emptable_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            emptable.PageIndex = e.NewPageIndex;
+            this.BindGrid();
         }
 
     }
